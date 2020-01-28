d7271a332b8c4fda7269fb726c90efa2a6e85544
Superxpdude <superxpdude@gmail.com>
2020-01-28 10:56:44 -0600

    Added the initial functioning version of the webhook module.

---
+++ cogs/Web.py
@@ -0,0 +1,47 @@
+import discord
+from discord.ext import commands, tasks
+import blueonblue
+from blueonblue.config import config
+from blueonblue.bot import bot as bluebot
+import logging
+log = logging.getLogger("blueonblue")
+
+from aiohttp import web
+import asyncio
+
+async def hello(request):
+	print(request)
+	print(request.headers["Host"])
+	print("Host" in request.headers)
+	return web.Response(text="Hello World")
+	
+async def msg(request):
+	gld = bluebot.get_guild(config["SERVER"]["ID"])
+	chnl = gld.get_channel(config["SERVER"]["CHANNELS"]["BOT"])
+	await chnl.send("Web message!")
+	return web.Response(text="Message sent")
+
+async def webserver_start(self):
+	app = web.Application()
+	app.add_routes([web.get('/', hello)])
+	app.add_routes([web.get('/msg', msg)])
+	runner = web.AppRunner(app)
+	await runner.setup()
+	site = web.TCPSite(runner, 'localhost', 8080)
+	self._webrunner = runner
+	await site.start()
+
+async def webserver_close(self):
+	await self._webrunner.cleanup()
+
+class Web(commands.Cog, name="Web"):
+	"""Web server test cog."""
+	def __init__(self,bot):
+		self.bot = bot
+		asyncio.get_event_loop().create_task(webserver_start(self))
+	
+	def cog_unload(self):
+		asyncio.get_event_loop().create_task(webserver_close(self))
+
+def setup(bot):
+	bot.add_cog(Web(bot))
