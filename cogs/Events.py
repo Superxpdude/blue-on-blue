e6d48c268fd67be5abf3f7b2474c67dc5e0c9756
Superxpdude <superxpdude@gmail.com>
2020-12-30 12:02:03 -0600

    Added the events command to pull events from a google calendar

--- cogs/Events.py
+++ cogs/Events.py
@@ -2,22 +2,23 @@ import discord
 from discord.ext import commands, tasks
 import blueonblue
 from blueonblue.config import config
-from datetime import datetime, timedelta
+from datetime import datetime, date
 import json
 from google.oauth2 import service_account
+from googleapiclient.discovery import build
 
 class Events(commands.Cog, name='Events'):
 
-    def __init__(self, bot):
-        self.bot = bot
-        self.event_channel = config['SERVER']['CHANNELS']['EVENT_ANNOUNCEMENTS']
+	def __init__(self, bot):
+		self.bot = bot
+		#self.event_channel = config['SERVER']['CHANNELS']['EVENT_ANNOUNCEMENTS']
 
-    #@commands.command(name='register_event')
-    #async def register_event(self, context, text_blob: str=""):
+	#@commands.command(name='register_event')
+	#async def register_event(self, context, text_blob: str=""):
 	
-		@commands.command(
+	@commands.command(
 		name="events",
-		brief="Grabs a list of upcoming events"
+		brief="Grabs a list of upcoming events",
 		aliases=['event']
 	)
 	@commands.max_concurrency(1,per=commands.BucketType.channel,wait=False)
@@ -28,74 +29,36 @@ class Events(commands.Cog, name='Events'):
 		credentials = service_account.Credentials.from_service_account_file(config["MISSIONS"]["SHEET"]["API_TOKEN_FILE"])
 		scoped_credentials = credentials.with_scopes(scope)
 		service = build("calendar","v3", credentials=credentials)
-		result = service.events().list(calendarId=config["EVENTS"]["CALENDAR"]["CALENDAR_ID"], timeMin=now, maxResults=10, singleEvents=True, orderBy='startTime').execute()
+		now = datetime.utcnow().isoformat() + "Z"
+		tz = "America/Winnipeg"
+		result = service.events().list(calendarId=config["EVENTS"]["CALENDAR"]["CALENDAR_ID"], timeMin=now, maxResults=10, singleEvents=True, orderBy='startTime', timeZone=tz).execute()
 		
-		# Probably don't need this
-		no_events = True
-		# Embed settings
-		
-		# Find the specific columns that we need on the sheet
-		# Google sheets refer to the first cell as cell 1, so we need to add one to our indexes
-		# TODO: Update this to handle errors properly
-		#col_date = mission_sheet.row_values(1).index("Date") + 1
-		#col_mission = mission_sheet.row_values(1).index("Mission") + 1
-		#col_map = mission_sheet.row_values(1).index("Map") + 1
-		#col_author = mission_sheet.row_values(1).index("Author(s)") + 1
-		#col_medical = mission_sheet.row_values(1).index("Medical") + 1
-		#col_contact = mission_sheet.row_values(1).index("Contact DLC") + 1
-		#col_notes = mission_sheet.row_values(1).index("Notes") + 1
-		
-#		for i in mission_sheet.get_all_values():
-#			try:
-#				datevar = datetime.strptime(i[0],"%Y-%m-%d")
-#			except:
-#				datevar = None
-#			if datevar is not None:
-#				if (
-#					i[2] != "" and 
-#					datetime.date(datevar) > 
-#					datetime.date(datetime.now() + timedelta(days=-1))
-#				):
-#					no_missions = False
-#					missionArr = [i[2],i[3]]
-#					missionURL = config["MISSIONS"]["WIKI"] + missionArr[0].replace(" ","_")
-#					if i[col_contact - 1] == "TRUE":
-#						missionContact = True
-#					else:
-#						missionContact = False
-#					if i[col_medical - 1] == "Advanced":
-#						missionAdvMed = True
-#					else:
-#						missionAdvMed = False
-#					missionTitle = datetime.date(datetime.strptime(i[0],"%Y-%m-%d")).strftime("%A") + ": " + i[0]
-#					
-#					# Set the embed colour
-#					if missionContact == True:
-#						missionColour = 0x00BB00 # Green to represent Contact DLC missions
-#					elif missionAdvMed == True:
-#						missionColour = 0xDF0000 # Red to represent Advanced medical missions
-#					else:
-#						missionColour = 0x2E86C1 # Start with the default blue
-#					
-#					# Append notes to the embed title
-#					if missionContact == True:
-#						missionTitle += ", Contact DLC"
-#					if missionAdvMed == True:
-#						missionTitle += ", Advanced Medical"
-#					
-#					embed = discord.Embed(title=missionTitle, color=missionColour)
-#					embed.add_field(name="Mission", value="[" + missionArr[0] + "](" + missionURL + ")", inline=True)
-#					if len(missionArr) > 1:
-#						embed.add_field(name="Map", value=missionArr[1], inline=True)
-#					else:
-#						embed.add_field(name="Map", value="None", inline=True)
-#					embed.add_field(name="Author", value=i[4], inline=True)
-#					if (i[col_notes - 1] != ""):
-#						embed.add_field(name="Notes", value=i[col_notes -1], inline=False)
-#					await ctx.send(embed=embed)
-#		
-#		if no_missions:
-#		 	await ctx.send("There aren't any missions scheduled right now. Why don't you schedule one?")
+		if len(result["items"]) > 0:
+			for e in result["items"]:
+				# We need to check both in case we have events that are set up as "All day events"
+				startTime = datetime.fromisoformat(e["start"]["dateTime"]) if "dateTime" in e["start"].keys() else None
+				startDate = date.fromisoformat(e["start"]["date"]) if "date" in e["start"].keys() else None
+				
+				if startTime is not None:
+					tz = startTime.utcoffset()
+					tzText = "CST" if (tz.days * 24) + (tz.seconds / 3600) <= -6 else "CDT"
+					embedTitle = startTime.strftime("%I:%M%p {tz}, %A: %Y-%m-%d").format(tz=tzText)
+				elif startDate is not None:
+					embedTitle = startDate.strftime("%A: %Y-%m-%d")
+				else:
+					embedTitle = "Date not found"
+				
+				embedColour = 0x9B59B6 # Discord purple
+				
+				embed = discord.Embed(title=embedTitle, color=embedColour)
+				if "summary" in e.keys():
+					embed.add_field(name="Event", value=e["summary"], inline=True)
+				if "description" in e.keys():
+					embed.add_field(name="Notes", value=e["description"], inline=False)
+				await ctx.send(embed=embed)
+				
+		else:
+			await ctx.send("There are no events scheduled at this time.")
 	
 def setup(bot):
 	bot.add_cog(Events(bot))
