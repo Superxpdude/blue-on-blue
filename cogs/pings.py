0e9bc7ab4fa26953fccfafa89909f1716ca4e43c
Superxpdude <superxpdude@gmail.com>
2022-02-09 10:48:26 -0600

    Added the ping command

--- cogs/pings.py
+++ cogs/pings.py
@@ -94,6 +94,13 @@ async def ping_delete(self, tag: str, guild: discord.Guild, cursor: asqlite.Curs
 	tag = tag.casefold()
 	await cursor.execute("DELETE FROM pings WHERE (server_id = :server_id AND ping_name = :ping)", {"server_id": guild.id, "ping": tag})
 
+async def ping_update_time(self, tag: str, guild: discord.Guild, cursor: asqlite.Cursor) -> bool:
+	"""Updates the "last used time" of a ping in the database."""
+	tag = tag.casefold()
+	time = round(datetime.now(timezone.utc).timestamp()) # Get the current time in timestamp format
+	ping_id = await ping_get_id(self, tag, guild, cursor)
+	await cursor.execute("UPDATE pings SET last_used_time = :time WHERE id = :ping", {"time": time, "ping": ping_id}) # Ping IDs are globally unique
+
 async def ping_add_user(self, tag: str, guild: discord.Guild, user: discord.Member, cursor: asqlite.Cursor) -> bool:
 	"""Adds a user to a ping.
 	Checks if the ping is an alias, and handles it accordingly.
@@ -154,12 +161,72 @@ async def ping_count_users(self, tag: str, guild: discord.Guild, cursor: asqlite
 		userCount = await cursor.fetchone()
 		return userCount[0] # This entry has to be on the return. The await can't be subscripted.
 
+async def ping_get_user_ids_by_id(self, id: int, cursor: asqlite.Cursor) -> list[int]:
+	"""Returns a list of user IDs present in a ping by using the ping ID.
+	Empty or invalid pings will return an empty list."""
+	userIDList = [] # Create our empty user ID list
+	# Grab data from the DB
+	await cursor.execute("SELECT user_id FROM ping_users WHERE ping_id = :ping", {"ping": id})
+	pingData = await cursor.fetchall()
+	for p in pingData:
+		if "user_id" in p.keys():
+			userIDList.append(p["user_id"])
+	# Return our list of userIDs
+	return userIDList
+
 class Pings(slash_util.Cog, name = "Pings"):
 	"""Ping users by a tag."""
 	def __init__(self, bot, *args, **kwargs):
 		super().__init__(bot, *args, **kwargs)
 		self.bot: blueonblue.BlueOnBlueBot = bot
 		self.db_check.start()
+
+	@commands.command()
+	async def ping(self, ctx: commands.Context, *, tag: str=""):
+		"""Pings all users associated with a specific tag.
+		Any text on a new line will be ignored. You can use this to send a message along with a ping."""
+		tag = tag.split("\n")[0]
+		san_check = sanitize_check(tag)
+		if san_check is not None: # Validate our tag first
+			await ctx.send(f"{ctx.author.mention}: {san_check}")
+			return
+		tag = tag.casefold() # String searching is case-sensitive
+
+		# Begin our DB section
+		async with self.bot.db_connection.cursor() as cursor:
+			ping_id = await ping_get_id(self, tag, ctx.guild, cursor) # Get the ID of the ping (or none if it doesn't exist)
+			if ping_id is None:
+				# Ping does not exist
+				response = f"{ctx.author.mention} This tag does not exist. Try `{ctx.prefix}pinglist` for a list of active pings."
+			else:
+				# Ping exists
+				pingUserIDs = await ping_get_user_ids_by_id(self, ping_id, cursor)
+				pingMentions: list[discord.Member] = []
+				for userID in pingUserIDs:
+					# Try to get the member object from the user ID
+					member = ctx.guild.get_member(userID)
+					if member is not None:
+						# Member is present in the guild
+						pingMentions.append(member.mention)
+
+				# Check to see if we have any valid members
+				if len(pingMentions) > 0:
+					# Ping has users
+					# Update the "last used time"
+					await ping_update_time(self, tag, ctx.guild, cursor)
+					response = f"{ctx.author.mention} has pinged `{tag}`: " + " ".join(pingMentions) # Create the ping message
+				else:
+					# Ping is empty
+					response = f"{ctx.author.mention} Ping `{tag}` appears to be empty. Performing cleanup." # Inform the user
+					await ping_delete(self, tag, ctx.guild, cursor)
+
+			await self.bot.db_connection.commit() # Write data to the database
+
+			# Send a response to the user.
+			if response is None:
+				response = f"{ctx.author.mention} It looks like there was an error with the command `{ctx.command.name}`"
+
+			await ctx.send(response)
 
 	@commands.command()
 	async def pingme(self, ctx: commands.Context, *, tag: str=""):
@@ -204,14 +271,14 @@ class Pings(slash_util.Cog, name = "Pings"):
 				# Add the user to the ping
 				success = await ping_add_user(self, tag, ctx.guild, ctx.author, cursor)
 				if success:
-					response = f"{ctx.author.mention} You have been added from to: `{tag}`"
+					response = f"{ctx.author.mention} You have been added to ping: `{tag}`"
 				else:
 					response = f"{ctx.author.mention} There was an error adding you to ping: `{tag}`"
 			await self.bot.db_connection.commit() # Write data to the database
 
 			# Send a response to the user.
 			if response is None:
-				response = f"{ctx.author.mention} It looks like there was an error with the command."
+				response = f"{ctx.author.mention} It looks like there was an error with the command `{ctx.command.name}`"
 
 			await ctx.send(response)
 
