9fefcb0ddc4f94eaa73fbd5ea9faf90f2cf78ee8
Superxpdude <superxpdude@gmail.com>
2022-02-22 18:02:18 -0600

    Added install and start files

---
+++ install.bat
@@ -0,0 +1,26 @@
+:: This batch file will handle installing the bot
+:: This will create a Virtual Environment, Activate it, and install the bot's prerequisites
+@echo off
+
+echo.
+echo		You are about to install Blue on Blue
+echo		Close this window to abort the install
+echo.
+echo		Press enter to continue
+echo.
+echo -----------------------------------------------------
+pause
+echo Creating virtual environment
+if exist venv\pyenv.cfg (
+	echo Virtual Environment Exists. Skipping...
+) else (
+	python -m venv ./venv/
+	echo Virtual Environment Created
+)
+echo Installing dependencies
+cmd /c venv\scripts\activate.bat ^
+& python -m pip install --upgrade pip ^
+& python start.py -i
+echo Blue on Blue is now installed
+echo Press enter to close
+pause
