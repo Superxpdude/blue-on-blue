5cb2860fb1daf36b29bd8d8c63d3906804c6a2ff
Superxpdude <superxpdude@gmail.com>
2023-03-28 18:12:32 -0500

    Removed unused imports in raffle.py

--- cogs/raffle.py
+++ cogs/raffle.py
@@ -1,10 +1,9 @@
 import discord
 from discord import app_commands
-from discord.ext import commands, tasks
+from discord.ext import commands
 
 import asyncio
 import datetime
-import math
 import random
 
 import blueonblue
