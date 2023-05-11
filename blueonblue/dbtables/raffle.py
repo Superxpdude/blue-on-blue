333e96cfd2a23d53ec24ef8329ee7c17b8632c8b
Superxpdude <superxpdude@gmail.com>
2023-05-11 15:46:20 -0500

    Added a function to increase a user's raffle weight

--- blueonblue/dbtables/raffle.py
+++ blueonblue/dbtables/raffle.py
@@ -4,6 +4,7 @@ class RaffleWeights(BaseTable):
 	"""Raffle Weights table class"""
 	_tableName = "raffle_weights"
 
+
 	async def getWeight(self, guildID: int, userID: int) -> float:
 		"""Returns the raffle weight of a user from the database
 
@@ -46,4 +47,30 @@ class RaffleWeights(BaseTable):
 				ON CONFLICT (server_id, user_id) DO UPDATE\
 				SET weight == :weight",
 				{"server_id": guildID, "user_id": userID, "weight": weight}
+			)
+
+
+	async def increaseRaffleWeight(self, guildID: int, userID: int, increase: float, maxWeight: float = 3.0) -> None:
+		"""Increase the raffle weight for a user.
+		Creates the raffle weight entry for the user if it does not exist
+
+		Parameters
+		----------
+		guildID : int
+			Guild ID
+		userID : int
+			User ID
+		increase : float
+			Increase amount
+		maxWeight : float
+			Maximum raffle weight
+		"""
+		async with self.db.connection.cursor() as cursor:
+			await cursor.execute(
+				"INSERT INTO raffle_weights (user_id, server_id, weight) VALUES (:user_id, :server_id, 1 + :increase)\
+				ON CONFLICT (user_id, server_id) DO\
+				UPDATE SET weight = MIN(:max, \
+					(SELECT weight FROM raffle_weights WHERE user_id = :user_id) + :increase\
+				)",
+				{"server_id": guildID, "user_id": userID, "increase": increase, "max": maxWeight}
 			)
