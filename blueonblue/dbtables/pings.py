e5eb808785d08aaa8b6b45375caf549383959794
Superxpdude <superxpdude@gmail.com>
2023-05-11 19:48:37 -0500

    Converted additional ping DB functions

--- blueonblue/dbtables/pings.py
+++ blueonblue/dbtables/pings.py
@@ -1,10 +1,11 @@
 from .base import BaseTable
+from datetime import datetime, timezone
 
 class Pings(BaseTable):
 	"""Ping table class"""
 	_tableName = "pings"
 
-	async def ping_exists(self, tag: str, guildID: int) -> bool:
+	async def exists(self, tag: str, guildID: int) -> bool:
 		"""Checks if a tag exists in the database for a specific guild.
 
 		Parameters
@@ -22,14 +23,14 @@ class Pings(BaseTable):
 		async with self.db.connection.cursor() as cursor:
 			await cursor.execute(
 				"SELECT id FROM :table_name WHERE server_id = :server_id AND ping_name = :ping",
-				{"table_name": self._tableName, "server_id": guildID, "ping": tag.casefold(),}
+				{"table_name": self._tableName, "server_id": guildID, "ping": tag.casefold()}
 			)
 			ping = await cursor.fetchone()
 			# Return false if ping is none, true otherwise
 			return not ping is None
 
 
-	async def ping_is_alias(self, tag: str, guildID: int) -> bool:
+	async def is_alias(self, tag: str, guildID: int) -> bool:
 		"""Checks if a tag is an alias for another tag
 
 		Parameters
@@ -47,14 +48,14 @@ class Pings(BaseTable):
 		async with self.db.connection.cursor() as cursor:
 			await cursor.execute(
 				"SELECT alias_for FROM :table_name WHERE server_id = :server_id AND ping_name = :ping AND alias_for IS NOT NULL",
-				{"table_name": self._tableName, "server_id": guildID, "ping": tag.casefold(),}
+				{"table_name": self._tableName, "server_id": guildID, "ping": tag.casefold()}
 			)
 			ping = await cursor.fetchone()
 			# Return false if ping is none, true otherwise
 			return not ping is None
 
 
-	async def ping_get_id(self, tag: str, guildID: int) -> int | None:
+	async def get_id(self, tag: str, guildID: int) -> int | None:
 		"""Gets the ID reference for a tag.
 		If the tag is an alias, get the alias ID. Otherwise, get the tag ID.
 		Returns None if the tag was not found.
@@ -74,7 +75,7 @@ class Pings(BaseTable):
 		async with self.db.connection.cursor() as cursor:
 			await cursor.execute(
 				"SELECT id, alias_for FROM :table_name WHERE server_id = :server_id AND ping_name = :ping",
-				{"table_name": self._tableName, "server_id": guildID, "ping": tag.casefold(),}
+				{"table_name": self._tableName, "server_id": guildID, "ping": tag.casefold()}
 			)
 			ping = await cursor.fetchone()
 
@@ -86,3 +87,124 @@ class Pings(BaseTable):
 				return ping["alias_for"]
 			else:
 				return ping["id"]
+
+
+	async def get_name(self, id: int, guildID: int) -> str | None:
+		"""Gets the name of a tag from its ID.
+		Returns None if the tag was not found.
+
+		Parameters
+		----------
+		id : int
+			Tag ID
+		guildID : int
+			Discord guild ID
+
+		Returns
+		-------
+		str | None
+			Tag name of ping if found
+		"""
+		async with self.db.connection.cursor() as cursor:
+			await cursor.execute(
+				"SELECT ping_name FROM :table_name WHERE server_id = :server_id AND id = :id",
+				{"table_name": self._tableName, "server_id": guildID, "id": id}
+			)
+			ping = await cursor.fetchone()
+
+			if ping is None: # Tag does not exist
+				return None
+			else:
+				return ping["ping_name"]
+
+
+	async def get_alias_names(self, id: int, guildID: int) -> tuple[str]:
+		"""Gets the names of all aliases for a given ping.
+		Returns a tuple containing all names.
+		When no aliases are present, the returned tuple will be empty.
+
+		Parameters
+		----------
+		id : int
+			Tag ID
+		guildID : int
+			Discord guild ID
+
+		Returns
+		-------
+		tuple[str]
+			Tuple of alias names
+		"""
+		async with self.db.connection.cursor() as cursor:
+			await cursor.execute(
+				"SELECT ping_name FROM :table_name WHERE server_id = :server_id AND alias_for = :id",
+				{"table_name": self._tableName, "server_id": guildID, "id": id}
+			)
+			aliasData = await cursor.fetchall()
+
+			aliasNames = []
+			for a in aliasData:
+				aliasNames.append(a["ping_name"])
+			return tuple(aliasNames)
+
+
+	async def create(self, tag: str, guildID: int) -> None:
+		"""Creates a ping using the given tag.
+
+		Parameters
+		----------
+		tag : str
+			Tag to use
+		guildID : int
+			Discord guild ID
+		"""
+		async with self.db.connection.cursor() as cursor:
+			await cursor.execute(
+				"INSERT INTO :table_name (server_id, ping_name, last_used_time) VALUES (:server_id, :ping, :time)",
+				{
+					"table_name": self._tableName,
+					"server_id": guildID,
+     				"ping": tag.casefold(),
+					"time": round(datetime.now(timezone.utc).timestamp())
+				}
+			)
+
+
+	async def delete_tag(self, tag: str, guildID: int) -> None:
+		"""Deletes the ping with a given tag.
+
+		Parameters
+		----------
+		tag : str
+			Ping tag to delete
+		guildID : int
+			Discord guild ID
+		"""
+		async with self.db.connection.cursor() as cursor:
+			await cursor.execute("DELETE FROM :table_name WHERE (server_id = :server_id AND ping_name = :ping)",
+				{
+					"table_name": self._tableName,
+					"server_id": guildID,
+     				"ping": tag.casefold()
+				}
+			)
+
+
+	async def delete_id(self, id: int, guildID: int) -> None:
+		"""Deletes the ping with a given ID.
+
+		Parameters
+		----------
+		id : int
+			Ping ID to delete
+		guildID : int
+			Discord guild ID
+		"""
+		async with self.db.connection.cursor() as cursor:
+			await cursor.execute("DELETE FROM :table_name WHERE (server_id = :server_id AND id = :id)",
+				{
+					"table_name": self._tableName,
+					"server_id": guildID,
+     				"id": id
+				}
+			)
