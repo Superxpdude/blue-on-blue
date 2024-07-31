b5a488f1c05784083566896d81261e1d7e80ee9b
Superxpdude <superxpdude@gmail.com>
2024-07-31 16:42:17 -0500

    Removed version check from __main__.py Auto-formatted __main__.py with Ruff

--- blueonblue/__main__.py
+++ blueonblue/__main__.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import sys
 import argparse
 import os
 
@@ -9,20 +8,14 @@ from .log import setup_logging
 
 import logging
 
-REQUIRED_VERSION = (3,10,0,"final")
 
 def main():
 	# Argument setup
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-d','--debug',action='store_true',dest="debug",help='Enable debug logging')
+	parser.add_argument(
+		"-d", "--debug", action="store_true", dest="debug", help="Enable debug logging"
+	)
 	args = parser.parse_args()
-
-	# Check python version
-	if (sys.version_info < REQUIRED_VERSION):
-		print("Python 3.10.0 or higher is required to run this bot." \
-		f"You are using version {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}")
-		input("Press ENTER to continue...")
-		exit()
 
 	# Start the bot
 	# Create required subfolders
@@ -32,7 +25,7 @@ def main():
 
 	# Set up logging
 	logLevel = logging.DEBUG if args.debug else logging.INFO
-	setup_logging(level = logLevel)
+	setup_logging(level=logLevel)
 	log = logging.getLogger("blueonblue")
 
 	bot = BlueOnBlueBot()
@@ -45,7 +38,8 @@ def main():
 
 	# Start the bot
 	log.info("Starting Blue on Blue")
-	bot.run(botToken, reconnect=True, log_handler = None)
+	bot.run(botToken, reconnect=True, log_handler=None)
+
 
 if __name__ == "__main__":
 	main()
