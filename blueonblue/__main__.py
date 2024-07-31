3cf38081c3135ba8db6a94ca1e013584e4086380
Superxpdude <superxpdude@gmail.com>
2024-07-31 15:24:29 -0500

    Streamline __main__ to only run the bot

--- blueonblue/__main__.py
+++ blueonblue/__main__.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
 import sys
 import argparse
-import subprocess
 import os
+
+from .bot import BlueOnBlueBot
+from .log import setup_logging
 
 import logging
 
-def install_dependencies():
-	subprocess.run([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+def main():
+	# Argument setup
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-d','--debug',action='store_true',dest="debug",help='Enable debug logging')
+	args = parser.parse_args()
 
-def start_bot(args: argparse.Namespace):
-	from .bot import BlueOnBlueBot
-	from .log import setup_logging
+	# Check python version
+	if (sys.version_info < (3,10,0,"final")):
+		print("Python 3.10.0 or higher is required to run this bot." \
+		f"You are using version {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}")
+		input("Press ENTER to continue...")
+		exit()
+
+	# Start the bot
+
 
 	# Create required subfolders
 	for f in ["config", "logs", "data"]:
@@ -36,47 +47,6 @@ def start_bot(args: argparse.Namespace):
 	# Start the bot
 	log.info("Starting Blue on Blue")
 	bot.run(botToken, reconnect=True, log_handler = None)
-
-def main():
-	# Argument setup
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-s','--start',action='store_true',dest="start",help='Run the bot immediately')
-	parser.add_argument('-d','--debug',action='store_true',dest="debug",help='Enable debug logging')
-	parser.add_argument('-i','--install',action='store_true',dest='install',help="Install the bot's dependencies")
-	args = parser.parse_args()
-
-	# Check python version
-	if (sys.version_info < (3,10,0,"final")):
-		print("Python 3.10.0 or higher is required to run this bot." \
-		f"You are using version {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}")
-		input("Press ENTER to continue...")
-		exit()
-
-	# If the installation argument is set. Install dependencies and exit.
-	if args.install:
-		install_dependencies()
-		exit()
-
-	if not args.start:
-		while True:
-			print(23 * "-","BLUE ON BLUE", 23 * "-")
-			print("1. Start Blue On Blue")
-			print("2. Install dependencies")
-			print("3. Exit")
-			print(60 * "-")
-			menu_choice = input("Enter your choice [1-3]: ")
-
-			if menu_choice == "1": # Start bot
-				break
-			elif menu_choice == "2": # Install dependencies
-				install_dependencies()
-			elif menu_choice == "3": # Exit
-				exit()
-			else:
-				input("Invalid option. Press ENTER to try again")
-
-	# Menu finished, start the bot
-	start_bot(args)
 
 if __name__ == "__main__":
 	main()
