1eed8fb0c629ac6f2e20ba9bda2418f5eebdf386
Superxpdude <superxpdude@gmail.com>
2024-07-31 16:41:58 -0500

    Explicitly defined bot and tree imports in __init__.py

--- blueonblue/__init__.py
+++ blueonblue/__init__.py
@@ -3,7 +3,7 @@
 All standard code should be placed within this folder, and be loaded by this file.
 Any code specific to a single command or cog should be placed within that cog."""
 
-from .bot import *
+from .bot import BlueOnBlueBot as BlueOnBlueBot, BlueOnBlueTree as BlueOnBlueTree
 from . import checks as checks
 from . import config as config
 from . import db as db
